06841e3847be310785fc27bf09a97696ba53fa83
chrishahn <chrishahn97@outlookcom>
2023-09-13 11:10:18 -0500

    added a while loop to keep the program running until you enter no

--- scrabble_scorer.py
+++ scrabble_scorer.py
@@ -1,5 +1,3 @@
-# this was a project from launch code that was done in JavaScript, I've redone it in Python
-
 old_point_structure = {
     1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
     2: ['D', 'G'],
@@ -23,9 +21,20 @@ def old_scrabble_scorer(word):
     return letter_points
 
 
-def initial_prompt():
-    word = input("Let's play some scrabble!\nEnter a word: ")
-    return word
+def initial_prompt(i):
+    while True:
+        if i >= 1:
+            word = input("Enter a word: ")
+            if word.isalpha():  # Check if the input consists only of letters
+                return word
+            else:
+                print("Invalid input. Please enter a word with letters only.")
+
+        word = input("Let's play some scrabble!\nEnter a word: ")
+        if word.isalpha():  # Check if the input consists only of letters
+            return word
+        else:
+            print("Invalid input. Please enter a word with letters only.")
 
 
 def simple_score(word):
@@ -61,14 +70,19 @@ scoring_algorithms = [simple_score, vowel_bonus_score, scrabble_score]
 
 
 def scorer_prompt(word):
-    choice = input(
-        "Which scoring algorithm would you like to use?\n"
-        "0 for Simple, Each letter is worth 1 point.\n"
-        "1 for Vowel Bonus, Vowels are 3 pts, consonants are 1 pt\n"
-        "or 2 for The traditional scoring for Scrabble.\nEnter 0, 1, or 2: "
-    )
-    choice = int(choice)
-    print(f'Score for "{word}": {scoring_algorithms[choice](word)}')
+    while True:
+        choice = input(
+            "Which scoring algorithm would you like to use?\n"
+            "0 for Simple, Each letter is worth 1 point.\n"
+            "1 for Vowel Bonus, Vowels are 3 pts, consonants are 1 pt\n"
+            "or 2 for The traditional scoring for Scrabble.\nEnter 0, 1, or 2: "
+        )
+        if choice.isdigit() and int(choice) in [0, 1, 2]:
+            choice = int(choice)
+            print(f'Score for "{word}": {scoring_algorithms[choice](word)}')
+            break
+        else:
+            print("Invalid input. Please enter 0, 1, or 2.")
 
 
 def transform():
@@ -85,8 +99,18 @@ new_point_structure = transform()
 
 
 def run_program():
-    user_input = initial_prompt()
-    scorer_prompt(user_input)
+    repeat = True
+
+    i = 0
+    while repeat:
+        user_input = initial_prompt(i)
+        scorer_prompt(user_input)
+        stop = input("Do you want to play again? \n (Y) or (N) ")
+        if stop.upper() == "N":
+            repeat = False
+            print("Ok goodbye")
+        i += 1
+
 
 
 run_program()
